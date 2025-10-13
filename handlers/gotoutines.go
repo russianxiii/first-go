063f0b71e4193ba7be2833fd8253353be1dbac4a
paveldrejzer <pavel.dreyzer@borkde.ru>
2025-10-13 20:38:14 +0300

    Добавляем ошибку и улучшаем код

--- handlers/gotoutines.go
+++ handlers/gotoutines.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 	"sync"
 
@@ -11,8 +12,21 @@ func sayHello(wg *sync.WaitGroup, mu *sync.Mutex, message string, messages *[]st
 	defer wg.Done()
 
 	mu.Lock()
-	(*messages)[idx] = message
+	var err error
+	if message == "User X: 404" {
+		err = simulateWriteError()
+	} else {
+		(*messages)[idx] = message
+	}
 	mu.Unlock()
+
+	if err != nil {
+		log.Println("Error writing message:", err)
+	}
+}
+
+func simulateWriteError() error {
+	return fmt.Errorf("ошибка записи")
 }
 
 func RoutinesHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,10 +37,11 @@ func RoutinesHandler(w http.ResponseWriter, r *http.Request) {
 
 	arr := []string{
 		"\n",
-		"User 1: hello!\n",
-		"User 2: hi!\n",
-		"User 1: how are you?\n",
-		"User 2: fine!\n",
+		"User 1: hello!",
+		"User 2: hi!",
+		"User 1: how are you?",
+		"User X: 404",
+		"User 2: fine!",
 	}
 
 	messages := make([]string, len(arr))
@@ -38,8 +53,11 @@ func RoutinesHandler(w http.ResponseWriter, r *http.Request) {
 
 	wg.Wait()
 
-	for _, msg := range messages {
-		w.Write([]byte(msg))
-		fmt.Println(msg)
+	for idx, msg := range messages {
+		if msg == "" {
+			continue
+		}
+		w.Write([]byte(msg + "\n"))
+		fmt.Printf("%d %s\n", idx, msg)
 	}
 }
